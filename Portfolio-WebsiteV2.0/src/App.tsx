1373f0a73d00f1f4743813042a568f595e7d6258
Prabhat <112978778+Gohan-18@users.noreply.github.com>
2023-02-14 20:35:51 +0530

    Header tried

--- Portfolio-WebsiteV2.0/src/App.tsx
+++ Portfolio-WebsiteV2.0/src/App.tsx
@@ -3,7 +3,34 @@ import './App.css';
 function App() {
   return (
     <>
-    <h1 className="text-3xl font-bold underline" >Hello world</h1>
+    <section> 
+      <header className='flex items-center justify-center rounded-b-xl w-screen header' >
+        <nav className='flex items-center justify-between w-screen p-5 '>
+            <ul className='flex items-center justify-between'>
+              <li><a href="index.html" className="header-brand">Prabhat.</a></li>
+              <li><a href="index.html" id="homeBtn">Home</a></li>
+              <li><a href="#technologies">Technologies</a></li>
+              <li><a href="#projects">Projects</a></li>
+            </ul>
+            <ul>
+              <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+      </header>
+    </section>
+
+    <section>
+      <main>
+
+      </main>
+    </section>
+
+    <section>
+      <footer>
+
+      </footer>
+    </section>
+
     </>
   )
 }
