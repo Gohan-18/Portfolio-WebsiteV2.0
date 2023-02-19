b6bc2ded63a7a286961351d1380f84c8dbcb2169
Prabhat <112978778+Gohan-18@users.noreply.github.com>
2023-02-19 22:59:25 +0530

    Added project video secrion

--- Portfolio-WebsiteV2.0/src/components/Projects.tsx
+++ Portfolio-WebsiteV2.0/src/components/Projects.tsx
@@ -1,19 +1,18 @@
 import React, { useContext, useEffect } from "react";
+import ReactPlayer from "react-player";
 import { AppContext } from "../App";
 import { projects } from "../utils/constants";
 
 const Projects = () => {
-
-  const { setActiveFilter } = useContext(AppContext)
+  const { setActiveFilter } = useContext(AppContext);
 
   useEffect(() => {
-    setActiveFilter('Projects')
-  }, [])
-  
+    setActiveFilter("Projects");
+  }, []);
 
   return (
     <>
-      <section className="z-5 flex w-full flex-col items-center justify-center bg-wht px-28 pt-24 pb-12 ">
+      <section className="z-5 flex w-full flex-col items-center justify-center bg-wht px-4 pt-24 pb-12 sm:px-10 md:px-14 ">
         <div className=" flex h-full w-full flex-col items-center justify-center gap-5">
           <h3 className="animate__animated animate__fadeInDown pt-4 font-catamaran text-3xl font-extrabold uppercase leading-3 text-indigo-800">
             Projects
@@ -22,26 +21,28 @@ const Projects = () => {
             return (
               <div
                 key={item.title}
-                className="animate__animated animate__fadeIn animate__delay-1s flex w-full max-w-4xl items-start justify-start gap-6 rounded-xl border-blue bg-gray-300 py-10 px-10 even:flex-row-reverse odd:border-r-4 even:border-l-4"
+                className="animate__animated animate__fadeIn animate__delay-1s flex w-full max-w-4xl flex-wrap items-start justify-start gap-6  border-blue bg-gray-300 py-6 px-4 odd:border-r-4 even:flex-row-reverse even:border-l-4 sm:rounded-xl sm:px-10 sm:py-10"
               >
-
-                <div className="flex items-center justify-center bg-wht h-full w-full flex-1">
+                <div className="flex h-full w-full items-center justify-center bg-wht lg:w-3/6">
                   {/* <h4 className="h-full w-full bg-wht p-20 font-poppins text-lg font-semibold text-slate-700 "></h4> */}
+                  <ReactPlayer url={item.youtubeLink} controls />
                 </div>
 
-                <div className="flex flex-1 flex-col items-start justify-center">
-                  <h4 className="font-poppins text-lg font-semibold text-slate-700">
+                <div className="flex flex-1 flex-col items-start justify-center gap-1 ">
+                  <h4 className="font-poppins text-base font-semibold text-slate-700 sm:text-lg">
                     {item.title}{" "}
                   </h4>
-                  <span className="font-poppins text-xs text-blue py-2 font-medium ">{item.date}</span>
-                  <h4 className="font-poppins text-sm font-medium text-slate-700">
+                  <span className=" pb-1 font-poppins text-xs font-medium text-blue">
+                    {item.date}
+                  </span>
+                  <h4 className="font-poppins text-xs font-medium text-slate-700 sm:text-sm">
                     {item.description}{" "}
                   </h4>
                   <div className="flex w-full flex-wrap items-center justify-start gap-2 pt-5 ">
                     {item.techStack.map((tech, index) => (
                       <span
                         key={index}
-                        className="font-poppins text-sm font-semibold bg-blue hover:bg-orange-600 transition-all duration-300 cursor-pointer text-wht px-1.5 py-0.5 rounded"
+                        className="cursor-pointer rounded-sm bg-blue px-1.5 py-0.5 font-poppins text-xs font-medium text-wht transition-all duration-300 hover:bg-orange-600 sm:rounded sm:text-sm sm:font-semibold"
                       >
                         {" "}
                         {tech}{" "}
@@ -49,7 +50,6 @@ const Projects = () => {
                     ))}
                   </div>
                 </div>
-
               </div>
             );
           })}
--- Portfolio-WebsiteV2.0/src/utils/constants.ts
+++ Portfolio-WebsiteV2.0/src/utils/constants.ts
@@ -5,7 +5,8 @@ export const projects = [
         description : "ShopMore, an E-Commerce website made using ReactJs.",
         techStack : [ 'React', 'Javascript', 'Material UI v5', 'Redux-Toolkit', 'HTML', 'NodeJs', 'Firebase', 'Formspree', 'Vite' ],
         liveLink : "https://shopmore-prabhat18.netlify.app/",
-        gitHubLink : "https://github.com/Gohan-18/Shopping-website"
+        gitHubLink : "https://github.com/Gohan-18/Shopping-website",
+        youtubeLink : "https://www.youtube.com/watch?v=5mJBauLcYp4&t=1648s"
     },
     {
         title : "Note Taking App",
@@ -13,7 +14,8 @@ export const projects = [
         description : "Made a full fledged note taking app using React. Made it to get some hands on creating CRUD based apps. The user can create a note, give it a title and the main content, update the content and title and also can delete it completely. They can also search any note by their title through the search bar given at the top. And yeah, almost forgot... the content won't get deleted after the page refresh(used LocalStorage to achieve that). Cheers!!",
         techStack : [ 'React', 'Javascript', 'HTML', 'CSS', 'NodeJs', 'Vite' ],
         liveLink : "https://note-app-prabhat18.netlify.app/",
-        gitHubLink : "https://github.com/Gohan-18/Note-Taking_App"
+        gitHubLink : "https://github.com/Gohan-18/Note-Taking_App",
+        youtubeLink : "https://www.youtube.com/watch?v=5mJBauLcYp4&t=1648s"
     },
     {
         title : "Chróma - An Image Searching App",
@@ -21,6 +23,7 @@ export const projects = [
         description : "It's an image searching webApp made with HTML, CSS and vanilla JavaScript. Here the user can search for any image of their liking by typing the desired keyword in the input box on the home page. It uses UnSplash API to fetch the searched image and show it to the user.",
         techStack : ['Javascript','HTML', 'CSS' ,'NodeJs'],
         liveLink : "https://chroma-animagesearchingapp.netlify.app/",
-        gitHubLink : "https://github.com/Gohan-18/Gallary-App/tree/main/Gallary-App"
+        gitHubLink : "https://github.com/Gohan-18/Gallary-App/tree/main/Gallary-App",
+        youtubeLink : "https://www.youtube.com/watch?v=5mJBauLcYp4&t=1648s"
     },
 ]
