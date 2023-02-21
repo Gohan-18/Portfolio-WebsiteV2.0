d8e7c540b2d6f037a5da2e5180f8b387021ce435
Prabhat <112978778+Gohan-18@users.noreply.github.com>
2023-02-21 22:54:41 +0530

    just some responsivness

--- Portfolio-WebsiteV2.0/src/components/About.tsx
+++ Portfolio-WebsiteV2.0/src/components/About.tsx
@@ -16,17 +16,15 @@ const About = () => {
 
   return (
     <>
-      <section className="z-5 flex lg:h-screen w-full flex-col items-center justify-center bg-dark-blue px-4 pt-24 pb-12 leading-6 transition-all duration-500 sm:px-10 md:bg-wht md:px-14 ">
+      <section className="z-5 flex w-full flex-col items-center justify-center bg-dark-blue px-4 pt-24 pb-12 leading-6 transition-all duration-500 sm:px-10 md:bg-wht md:px-14 lg:h-screen ">
         <div className=" flex h-full w-full flex-col items-center justify-center">
-          <h3 className="animate__animated animate__fadeInDown pt-2 pb-4 text-center font-catamaran text-2xl sm:text-3xl font-extrabold uppercase leading-3 text-wht transition-all duration-500 md:text-indigo-800">
+          <h3 className="animate__animated animate__fadeInDown pt-2 pb-4 text-center font-catamaran text-2xl font-extrabold uppercase leading-3 text-wht transition-all duration-500 sm:text-3xl md:text-indigo-800">
             {`< About />`}
           </h3>
           <div className="animate__animated animate__fadeIn animate__delay-1s flex h-full w-full max-w-4xl flex-col items-center justify-center gap-2 rounded border-l-4 border-r-4 border-orange-600 bg-wht py-6 px-4 text-sm transition-all duration-500 sm:text-base md:gap-2 md:rounded-lg md:border-indigo-800 md:bg-gray-300 md:px-10 md:py-10">
-            <p className="max-w-lg text-center font-poppins font-medium text-sm md:text-base capitalize text-slate-700">
+            <p className="max-w-sm text-center font-poppins text-xs font-medium capitalize text-slate-700 sm:text-sm md:max-w-lg md:text-base">
               Hey there, 👋 my name is{" "}
-              <span className="font-semibold text-orange-600 ">
-                Prabhat
-              </span>{" "}
+              <span className="font-semibold text-orange-600 ">Prabhat</span>{" "}
               and i'm a{" "}
               <span className="font-semibold text-orange-600">
                 fullstack MERN webDeveloper
@@ -37,7 +35,7 @@ const About = () => {
               application in 2022 and currently working at wipro as scholar
               trainee.
             </p>
-            <p className="max-w-lg text-center font-poppins font-medium text-sm md:text-base capitalize text-slate-700">
+            <p className="max-w-sm text-center font-poppins text-xs font-medium capitalize text-slate-700 sm:text-sm md:max-w-lg md:text-base">
               I can help you in creating frontEnd webApps using{" "}
               <span className="text-indigo-800 ">ReactJs/Ts</span>, state
               management tool like{" "}
@@ -47,13 +45,13 @@ const About = () => {
               frontEnd UI using modern UI libraries like{" "}
               <span className="text-indigo-800 ">Material ui v5/Tailwind</span>.
             </p>
-            <p className="max-w-lg text-center font-poppins font-medium text-sm md:text-base capitalize text-slate-700">
+            <p className="max-w-sm text-center font-poppins text-xs font-medium capitalize text-slate-700 sm:text-sm md:max-w-lg md:text-base">
               currently I am looking for front-End opportunities (preferably
               around React tech stack). so if you are looking for someone with
               mentioned tech skill,{" "}
               <Link
                 onClick={() => setActiveFilter("Contact")}
-                className=" inline-block rounded border-2 border-blue bg-blue px-1.5 py-0.5 font-poppins text-sm font-semibold  text-wht transition-all duration-500 hover:border-slate-100 hover:bg-slate-100 hover:text-indigo-800 "
+                className=" inline-block rounded border-2 border-blue bg-blue px-1.5 py-0.5 font-poppins text-xs font-semibold text-wht  transition-all duration-500 hover:border-slate-100 hover:bg-slate-100 hover:text-indigo-800 sm:text-sm "
                 to={"/Contact"}
               >
                 Contact Me
