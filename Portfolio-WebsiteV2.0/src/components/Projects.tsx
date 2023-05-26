63809a3778fd2f155fabbfd51c43c6c30dced56a
Prabhat <pkjha187@gmail.com>
2023-05-26 14:55:08 +0530

    Added Andro-Setup full details

--- Portfolio-WebsiteV2.0/src/components/Projects.tsx
+++ Portfolio-WebsiteV2.0/src/components/Projects.tsx
@@ -3,7 +3,7 @@ import ReactPlayer from "react-player";
 import { AppContext } from "../App";
 import { miniProjects, projects } from "../utils/constants";
 import { AiFillGithub } from "react-icons/ai";
-import { AiOutlineLink  } from "react-icons/ai";
+import { AiOutlineLink } from "react-icons/ai";
 
 const Projects = () => {
   const { setActiveFilter } = useContext(AppContext);
@@ -23,7 +23,7 @@ const Projects = () => {
             return (
               <div
                 key={item.title}
-                className="animate__animated animate__fadeIn animate__delay-1s flex w-full max-w-4xl flex-wrap items-start justify-start gap-6 rounded border-orange-400 bg-wht py-6 px-4 transition-all duration-500 odd:border-r-4 even:border-l-4 md:odd:border-r-8 even:flex-row-reverse md:even:border-l-8 md:rounded-lg md:bg-gray-200 md:px-10 md:py-10"
+                className="animate__animated animate__fadeIn animate__delay-1s flex w-full max-w-4xl flex-wrap items-start justify-start gap-6 rounded border-orange-400 bg-wht py-6 px-4 transition-all duration-500 odd:border-r-4 even:flex-row-reverse even:border-l-4 md:rounded-lg md:bg-gray-200 md:px-10 md:py-10 md:odd:border-r-8 md:even:border-l-8"
               >
                 <div className="flex h-full w-full items-center justify-center bg-wht lg:w-3/6">
                   {/* <h4 className="h-full w-full bg-wht p-20 font-poppins text-lg font-semibold text-slate-700 "></h4> */}
@@ -31,38 +31,38 @@ const Projects = () => {
                 </div>
 
                 <div className="flex flex-1 flex-col items-start justify-center gap-1 ">
-                  <div  >
+                  <div>
                     <a
                       title="Live Project"
                       href={item.liveLink}
                       target="_blank"
-                      className="font-poppins capitalize text-base font-semibold text-slate-700 transition-all duration-500 hover:text-orange-500 sm:text-lg "
+                      className="font-poppins text-base font-semibold capitalize text-slate-700 transition-all duration-500 hover:text-orange-500 sm:text-lg "
                     >
                       {item.title}{" "}
                     </a>
-                    <span className="flex items-center justify-start gap-2 w-full" >
-                    <a
-                      className="flex h-fit w-fit"
-                      title="Live Project"
-                      href={item.liveLink}
-                      target="_blank"
-                    >
-                      <AiOutlineLink className="cursor-pointer rounded-full text-slate-600 transition-all duration-500 hover:text-orange-500 " />
-                    </a>
-                    <a
-                      className="flex h-fit w-fit"
-                      title="GitHub"
-                      href={item.gitHubLink}
-                      target="_blank"
-                    >
-                      <AiFillGithub className="h-4 w-8 cursor-pointer rounded-full bg-slate-600 p-0.5 text-wht transition-all duration-500 hover:bg-orange-500 " />{" "}
-                    </a>
+                    <span className="flex w-full items-center justify-start gap-2">
+                      <a
+                        className="flex h-fit w-fit"
+                        title="Live Project"
+                        href={item.liveLink}
+                        target="_blank"
+                      >
+                        <AiOutlineLink className="cursor-pointer rounded-full text-slate-600 transition-all duration-500 hover:text-orange-500 " />
+                      </a>
+                      <a
+                        className="flex h-fit w-fit"
+                        title="GitHub"
+                        href={item.gitHubLink}
+                        target="_blank"
+                      >
+                        <AiFillGithub className="h-4 w-8 cursor-pointer rounded-full bg-slate-600 p-0.5 text-wht transition-all duration-500 hover:bg-orange-500 " />{" "}
+                      </a>
                     </span>
                   </div>
                   <span className=" pb-1 font-poppins text-xs font-medium text-blue">
                     {item.date}
                   </span>
-                  <h4 className="font-poppins text-xs text-justify font-medium text-slate-600 md:text-xms">
+                  <h4 className="text-justify font-poppins text-xs font-medium text-slate-600 md:text-xms">
                     {item.description}{" "}
                   </h4>
                   <div className="flex w-full flex-wrap items-center justify-start gap-2 pt-5 ">
@@ -86,12 +86,12 @@ const Projects = () => {
           <h3 className="animate__animated animate__fadeInDown animate__delay-1s py-4 text-center font-catamaran text-2xl font-extrabold uppercase leading-3 text-wht sm:text-3xl">
             {`< Mini Projects />`}
           </h3>
-          <div className="grid grid-cols-1 max-w-5xl xl:grid-cols-3 md:grid-cols-2 w-full justify-items-center content-center gap-5" >
+          <div className="grid w-full max-w-5xl grid-cols-1 content-center justify-items-center gap-5 md:grid-cols-2 xl:grid-cols-3">
             {miniProjects.map((item) => {
               return (
-                <div 
+                <div
                   key={item.title}
-                  className="animate__animated animate__fadeIn animate__delay-1s  relative flex flex-col w-full flex-wrap items-start justify-start gap-1 rounded border-orange-400 border-4 bg-wht py-6 px-4 transition-all duration-500 rounded-tr-custom md:bg-gray-200 md:px-8"
+                  className="animate__animated animate__fadeIn animate__delay-1s  relative flex w-full flex-col flex-wrap items-start justify-start gap-1 rounded rounded-tr-custom border-4 border-orange-400 bg-wht py-6 px-4 transition-all duration-500 md:bg-gray-200 md:px-8"
                 >
                   <a
                     className="flex h-fit w-fit "
@@ -99,9 +99,9 @@ const Projects = () => {
                     href={item.githubLink}
                     target="_blank"
                   >
-                    <AiFillGithub className="h-8 w-8 absolute cursor-pointer -top-4 left-5 rounded-md bg-slate-600 hover:scale-110 p-1.5 text-wht transition-all duration-500 hover:bg-orange-500" />{" "}
+                    <AiFillGithub className="absolute -top-4 left-5 h-8 w-8 cursor-pointer rounded-md bg-slate-600 p-1.5 text-wht transition-all duration-500 hover:scale-110 hover:bg-orange-500" />{" "}
                   </a>
-                  <div className="" >
+                  <div className="">
                     <a
                       title="Live Project"
                       href={item.liveLink}
@@ -114,13 +114,12 @@ const Projects = () => {
                   <span className=" pb-1 font-poppins text-xs font-medium text-blue">
                     {item.date}
                   </span>
-                  <h4 className="font-poppins text-xs text-justify leading-4 font-medium text-slate-600 md:text-xms">
+                  <h4 className="text-justify font-poppins text-xs font-medium leading-4 text-slate-600 md:text-xms">
                     {item.description}{" "}
                   </h4>
                 </div>
-              )
+              );
             })}
-
           </div>
         </div>
       </section>
